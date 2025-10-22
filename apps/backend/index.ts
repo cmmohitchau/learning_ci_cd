de0b6dc4150029a4f6b11c343b62d3fa0c4afb9e
cmmohitchau <cmmohitchau@gmail.com>
2025-10-22 16:27:31 +0530

    added logging user

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -13,6 +13,8 @@ app.post("/" , async(req , res) => {
             password : "mohit123backend"
         }
     })
+    console.log(user);
+    
 
     res.status(200).json({
         message : "fetched",
